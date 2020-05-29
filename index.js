60b5d1e4aeb6f92a57f0179b029a640758c93ac8
ssolomero <ssolomero@gmail.com>
2020-05-29 19:11:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".project-info-container").slideDown("slow");
+});
+
+
+// var coverSectionHeight = $("#cover-section").height();
+// var coverHeight = $(".cover").height();
+// $(".cover").css("padding-top", coverSectionHeight/2 - coverHeight/2);
